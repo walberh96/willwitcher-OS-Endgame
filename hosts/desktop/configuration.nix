5fd7c281209c6a18b497282d43a91309e4736edf
Willwitcher <willgamedevelopment@gmail.com>
2025-11-06 01:59:31 -0800

    "Changed ly for Greetd"

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -85,14 +85,18 @@
   nixpkgs.config.allowUnfree = true;
 
   ########################
-  # Display Manager: LY
+  # Display Manager
   ########################
-  services.displayManager.ly.enable = true;
+ services.greetd.enable = true;
 
-  # Asegura sesión por defecto a Hyprland
-  services.displayManager.defaultSession = "hyprland";
-  # Por si LY necesita ver el .desktop de Hyprland explícitamente:
-  services.displayManager.sessionPackages = [ pkgs.hyprland ];
+  # Instala ReGreet y deja un regreet.toml por defecto en /etc/greetd/
+  programs.regreet.enable = true;
+
+  # Lanza ReGreet dentro de un compositor Wayland mínimo (cage)
+  services.greetd.settings.default_session = {
+    user = "greeter";
+    command = "${pkgs.cage}/bin/cage -s -mlast -- ${pkgs.regreet}/bin/regreet";
+  }; 
 
   ########################
   # Hyprland
