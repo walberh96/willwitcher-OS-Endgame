14a2ac9625aadb1c4dcb88b2f892efa595df0405
Willwitcher <willgamedevelopment@gmail.com>
2025-11-26 23:52:34 -0800

    This is the new version(beta) using stow to manage dotfiles

--- hosts/desktop/dotfiles/.config/nvim/lua/plugins/catppuccin_mocha.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "catppuccin/nvim",  -- Plugin name
-    name = "catppuccin", -- You can still specify a name
-    priority = 1000, -- Plugin priority
-    config = function()
-        vim.cmd("colorscheme catppuccin-mocha")
-    end
-  }
--- hosts/desktop/system_packages.nix
+++ hosts/desktop/system_packages.nix
@@ -36,6 +36,7 @@ gawk
 gnugrep
 papirus-icon-theme
 gnused
+stow
 
 #------------------------------------------------------
 ]
